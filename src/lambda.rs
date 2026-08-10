26d4f36f87ba7440d8bbfce036e2a5531b2963b7
Patrick Wells <patrick.wells.95@gmail.com>
2026-08-10 16:46:44 -0500

    Implement lambda expressions

---
+++ src/lambda.rs
@@ -0,0 +1,63 @@
+use crate::atom::Value;
+use crate::env::Env;
+use crate::eval::EvalError;
+use crate::special::progn;
+use std::cell::RefCell;
+use std::iter::zip;
+use std::rc::Rc;
+
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) struct Lambda {
+    params: Vec<String>,
+    body: Rc<Value>,
+    env: Rc<RefCell<Env>>,
+}
+
+impl Lambda {
+    fn new(params: Vec<String>, body: Rc<Value>, env: Rc<RefCell<Env>>) -> Self {
+        Lambda { params, body, env }
+    }
+    pub(crate) fn apply(&self, pars: Vec<Rc<Value>>) -> Result<Rc<Value>, EvalError> {
+        if pars.len() != self.params.len() {
+            return Err(EvalError::Unknown);
+        }
+        let mut new_env = Env::make_child(self.env.clone());
+        for (name, par) in zip(self.params.iter(), pars.iter()) {
+            new_env.insert(name, par)
+        }
+        let value = progn(self.body.as_ref(), Rc::new(RefCell::new(new_env)))?;
+        Ok(Rc::new(value))
+    }
+}
+
+fn as_cons(value: &Value) -> Result<(Rc<Value>, Rc<Value>), EvalError> {
+    match value {
+        Value::List((left, right)) => Ok((left.clone(), right.clone())),
+        _ => Err(EvalError::Unknown),
+    }
+}
+
+pub(crate) fn lambda(value: &Value, env: Rc<RefCell<Env>>) -> Result<Value, EvalError> {
+    let (params_expr, body) = as_cons(value)?;
+    match body.as_ref() {
+        Value::Nil => return Err(EvalError::Unknown),
+        _ => {}
+    }
+    let parameters = collect_names(params_expr.as_ref())?;
+    Ok(Value::Lambda(Lambda::new(parameters, body, env.clone())))
+}
+
+fn collect_names(value: &Value) -> Result<Vec<String>, EvalError> {
+    let (mut name, mut rest) = as_cons(value)?;
+    let mut names = Vec::new();
+    loop {
+        match name.as_ref() {
+            Value::Id(id) => names.push(id.clone()),
+            _ => return Err(EvalError::Unknown),
+        }
+        (name, rest) = match rest.as_ref() {
+            Value::Nil => return Ok(names),
+            _ => as_cons(rest.as_ref())?,
+        }
+    }
+}
